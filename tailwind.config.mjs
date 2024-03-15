820750149ba4318f568222adf9978f0cf28fbdd8
McGonigleDevOps <patrick@mcgonigle.studio>
2024-03-15 13:27:32 +0000

    v.1.0

---
+++ tailwind.config.mjs
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			'sans': ['Montserrat', 'sans-serif']
+		},
+		extend: {
+			fontFamily: {
+				'brand-cursive': ['Pacifico','cursive'],
+			}
+		},
+	},
+	daisyui: {
+		themes: [
+		  {
+			myLightTheme: {
+   				"primary": "#e11d48",
+				"secondary": "#e879f9",
+			   	"accent": "#00ffff",
+			   	"neutral": "#ff00ff",
+				"base-100": "#f3f4f6",
+				"info": "#0000ff",
+				"success": "#00ff00",
+				"warning": "#fde047",
+				"error": "#ff0000",
+			},
+		  },
+		],
+	  },
+	  plugins: [require("daisyui")],
+}
